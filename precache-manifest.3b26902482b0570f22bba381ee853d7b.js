29bf5f05d07c90caa549a97d2ce1b92037981697
jessica bradham <jessijoke@yahoo.com>
2020-05-02 00:42:09 -0400

    initial commit

---
+++ precache-manifest.3b26902482b0570f22bba381ee853d7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6d954e795fc5d36fa2d5a14349b3cd",
+    "url": "/JessicaJoy/Robofriends/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/JessicaJoy/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9799d2b69f4556c4e1c9",
+    "url": "/JessicaJoy/Robofriends/static/css/main.5088a9cc.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/JessicaJoy/Robofriends/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/JessicaJoy/Robofriends/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9799d2b69f4556c4e1c9",
+    "url": "/JessicaJoy/Robofriends/static/js/main.b44551f5.chunk.js"
+  },
+  {
+    "revision": "ab8272b06420c6d10124",
+    "url": "/JessicaJoy/Robofriends/static/js/runtime-main.ae8d1ae1.js"
+  }
+]);
